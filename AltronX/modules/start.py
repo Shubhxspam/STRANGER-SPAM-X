db76328aa768b8d1c61a3e592fb076c7196db98c
Shubhxspam <164321506+Shubhxspam@users.noreply.github.com>
2025-02-20 20:53:11 +0530

    Update start.py

--- AltronX/modules/start.py
+++ AltronX/modules/start.py
@@ -8,37 +8,29 @@ PythonButton = [
         Button.inline("⚡️𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦⚡️", data="help_back")
         ],
         [
-        Button.url("⚡️𝗖𝗛𝗔𝗡𝗡𝗘𝗟⚡️", "https://t.me/SHIVANSH474"),
-        Button.url("⚡️𝗦𝗨𝗣𝗣𝗢𝗥𝗧⚡️", "https://t.me/MASTIWITHFRIENDSXD")
+        Button.url("⚡️𝗠𝗢𝗩𝗜𝗘𝗦⚡️", "https://t.me/MoviesWDs_bot"),
+        Button.url("⚡️𝗦𝗨𝗣𝗣𝗢𝗥𝗧⚡️", "https://t.me/Mrshubh_1227")
         ],
         [
-        Button.url("⚡️𝗥𝗘𝗣𝗢⚡️", "https://github.com/itzshukla/STRANGER-SPAM-X/fork")
+        Button.url("⚡️𝗢𝗪𝗡𝗘𝗥⚡️", "https://t.me/Demonxcoder")
         ]
         ]
 
 
 @MK1.on(events.NewMessage(pattern="/start"))
 @MK2.on(events.NewMessage(pattern="/start"))
-@MK3.on(events.NewMessage(pattern="/start"))
-@MK4.on(events.NewMessage(pattern="/start"))
-@MK5.on(events.NewMessage(pattern="/start"))
-@MK6.on(events.NewMessage(pattern="/start"))
-@MK7.on(events.NewMessage(pattern="/start"))
-@MK7.on(events.NewMessage(pattern="/start"))
-@MK8.on(events.NewMessage(pattern="/start"))
-@MK9.on(events.NewMessage(pattern="/start"))
-@MK10.on(events.NewMessage(pattern="/start"))
+
 async def start(event):              
     if event.is_private:
         AltBot = await event.client.get_me()
         BotName = AltBot.first_name
         BotId = AltBot.id
         TEXT = f"**𝗛𝗘𝗬 [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n𝗜 𝗔𝗠  [{BotName}](tg://user?id={BotId})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **✦ 𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗗 𝗕𝗬 :~ [𝗦𝗛𝗜𝗩𝗔𝗡𝗦𝗛](https://t.me/SHIVANSHDEVS)**\n\n"
-        TEXT += f"» **𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥 𝗦𝗣𝗔𝗠 𝗩𝗘𝗥𝗦𝗜𝗢𝗡 :** `3.2`\n"
+        TEXT += f"» **✦ 𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗗 𝗕𝗬 :~ [𝗦𝗛𝗨𝗕𝗛](https://t.me/Demonxcoder)**\n\n"
+        TEXT += f"» **𝗦𝗛𝗨𝗕𝗛 𝗦𝗣𝗔𝗠 𝗩𝗘𝗥𝗦𝗜𝗢𝗡 :** `3.2`\n"
         TEXT += f"» **𝗧𝗘𝗟𝗘𝗧𝗛𝗢𝗡 𝗩𝗘𝗥𝗦𝗜𝗢𝗡:** `{telethon.__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                 event.chat_id,
-                "https://telegra.ph/file/05522e13c97752efe5e75.png",
+                "https://graph.org/file/db0fbc02a08c2a28349f1-3671ff24b68aa73e82.jpg",
                 caption=TEXT, 
                 buttons=PythonButton)
