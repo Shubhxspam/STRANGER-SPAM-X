b45df8e6f3cbab20392d33a367adcf7bdaba028c
Shubhxspam <164321506+Shubhxspam@users.noreply.github.com>
2025-02-20 20:42:45 +0530

    Update leave.py

--- AltronX/modules/leave.py
+++ AltronX/modules/leave.py
@@ -1,17 +1,10 @@
-from config import MK1, MK2, MK3, MK4, MK5, MK6, MK7, MK8, MK9, MK10, SUDO_USERS, CMD_HNDLR as hl
+from config import MK1, MK2,  SUDO_USERS, CMD_HNDLR as hl
 from telethon.tl.functions.channels import LeaveChannelRequest
 from telethon import events
 
 @MK1.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
 @MK2.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
-@MK3.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
-@MK4.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
-@MK5.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
-@MK6.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
-@MK7.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
-@MK8.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
-@MK9.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
-@MK10.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+
 async def leave(e):
     if e.sender_id in SUDO_USERS:
         mkl = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
